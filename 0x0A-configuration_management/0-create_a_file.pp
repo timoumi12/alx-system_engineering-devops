b18e99bb7b3499af6eb2b51d1642465862cc5cd4
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-22 11:01:50 +0100

    first task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# Define a Puppet class to manage the file
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+Puppet is a configuration management and automation tool that is used to manage and automate the provisioning and configuration of servers and infrastructure.
