972bbad3cb9557a603b94b6f1bf0cc863ab7e136
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-27 21:09:53 +0100

    optional task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,12 @@
 
 exec {'install':
   provider => shell,
-  command  => 'apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" > /var/www/html/index.html' ; echo "server {\n\tlisten 80;\n\troot\t/etc/nginx/html;\n\tindex  index.html;\n\tlocation /redirect_me {\n\t\treturn 301 www.linkedin.com/in/iheb-timoumi-576a311b0;\n\t}\n}" > /etc/nginx/sites-available/default ;sudo service nginx start',
+  command  => 'echo "server {
+      listen 80;
+      root   /etc/nginx/html;
+      index  index.html;
+      location /redirect_me {
+        return 301 www.linkedin.com/in/iheb-timoumi-576a311b0;
+      }
+}" > /etc/nginx/sites-available/default'
 }
