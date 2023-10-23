f916b04185d64d61476a128ed23de8d41bce9f1b
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-23 12:55:59 +0100

    hot fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     with open("{}.csv".format(id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for i in todos_res:
-            row = [i.get("userId"),
-                user_res.get("name"),
-                i.get("completed"),
-                i.get("title")]
+            row = [id,
+                   user_res.get("username"),
+                   i.get("completed"),
+                   i.get("title")]
             writer.writerow(row)
