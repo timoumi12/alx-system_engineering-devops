2af2134899a33fca13a7dd67b48fe92ed0622066
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-23 11:53:00 +0100

    gather data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com"
+    user_res = requests.get("{}/users/{}".format(url, id)).json()
+    todos_res = requests.get("{}/todos".format(url), params={"userId": id}).json()
+    c_todos = []
+    for i in todos_res:
+        if i.get("completed") == True:
+            c_todos.append(i)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(id, len(c_todos), len(todos_res)))
+    for i in c_todos:
+        print("\t {}".format(i.get("title")))
