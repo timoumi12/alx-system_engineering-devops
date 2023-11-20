0d7ea94a342c2591eb6f3638072bad9b7fde46b1
timoumi12 <ihebtimoumi1@gmail.com>
2023-11-20 10:12:46 +0100

    hot fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix limit file
 exec { 'Correct file and restart':
-  provider => "shell",
-  command  => 'sudo sed -i \'s/15/30000/\' /etc/default/nginx && sudo service nginx restart'
+  provider => shell,
+  command  => "sudo sed -i 's/15/30000/' /etc/default/nginx && sudo service nginx restart"
 }
